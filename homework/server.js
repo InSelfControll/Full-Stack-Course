5a3d3fd7dbb274cce98f23c2a4e59d6caa9717c3
InSelfControll <admin@hostme.co.il>
2020-11-23 16:03:24 +0000

    0

--- homework/server.js
+++ homework/server.js
@@ -1,9 +1,14 @@
+const fs = require("fs");
 const app = require("express")();
 
 app.use(require("connect-history-api-fallback")());
 
 app.get("/*", (req, res) => {
-  res.sendFile(__dirname + req.path);
+  const paths = req.path.split("/");
+  while (!fs.existsSync(__dirname + "/" + paths.join("/"))) {
+    paths.shift();
+  }
+  res.sendFile(__dirname + "/" + paths.join("/"));
 })
 
 const PORT = process.env.PORT || 8080;
