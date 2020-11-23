5c8a3120f84d0265d6a09182ef11be4f4287bbd6
BuildTools <unconfigured@null.spigotmc.org>
2020-11-23 18:34:31 +0200

    --- Auto-Git Commit ---

--- homework/ui.js
+++ homework/ui.js
@@ -5,21 +5,20 @@ const elCpus = document.querySelectorAll(".cpus");
 
 function init() {
   if (!location.pathname.startsWith("/gpu") || !location.pathname.startsWith("/cpu")) {
-    return;
+    return ;
   }
 
   const id = location.pathname.split("/").pop() - 1;
-  if (!isNaN(id) && !elGpus[id]) {
-    return;
-  } else if (!isNaN[id] && !elCpus[id]){
-    return alert("Incorrect");
+  if (!isNaN(id) && !elGpus[id] || !isNaN[id] && !elCpus[id]) {
+    return alert("Incorrect option");
   }
 
   const top = elGpus[id].getBoundingClientRect().top;
   scroll(0, top)
+
+  const bottom = elCpus[id].getBoundingClientRect().bottom;
+  scroll(1, bottom)
 }
-  const top = elCpus[id].getBoundingClientRect().bottom;
-  scroll(0, bottom)
-}
+
 
 init();
