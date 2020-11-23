e51d503a49c1829e058300e12992afc9cd143711
BuildTools <unconfigured@null.spigotmc.org>
2020-11-23 22:03:39 +0200

    --- Auto-Git Commit ---

--- homework/ui.js
+++ homework/ui.js
@@ -1,22 +1,22 @@
 "use strict";
 
-const elGpus = document.querySelectorAll(".amd-gpus");
-const elCpus = document.querySelectorAll(".amd-cpus");
+const elAll = document.querySelectorAll(".options");
+//const elCpus = document.querySelectorAll(".amd-cpus");
 
 function init() {
-  if (!location.pathname.startsWith("/gpu") && !location.pathname.startsWith("/cpu")) {
+  if (!location.pathname.startsWith("/options")) {
     return ;
   }
 
-  const id = location.pathname.split("/Subjects").pop() - 1;
-  if (!isNaN(id) && !elGpus[id] && !isNaN[id] && !elCpus[id]) {
+  const id = location.pathname.split("/subjects").pop() - 1;
+  if (!isNaN(id) && !elAll[id]) {
     return alert("Incorrect option");
   }
 
-  const top = elGpus[id].getBoundingClientRect().top;
+  const top = elAll[id].getBoundingClientRect().top;
   scroll(0, top)
 
-  const bottom = elCpus[id].getBoundingClientRect().bottom;
+  const bottom = elAll[id].getBoundingClientRect().bottom;
   scroll(0, bottom)
 }
 
