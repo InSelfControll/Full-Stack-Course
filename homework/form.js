68ef04c135d8f5e1f6d39d3c518d808381a3c523
BuildTools <unconfigured@null.spigotmc.org>
2020-11-24 13:36:30 +0200

    --- Auto-Git Commit ---

---
+++ homework/form.js
@@ -0,0 +1,9 @@
+
+    function readText (form) {
+        TestVar = form.inputbox.value;
+    alert ("You typed: " + TestVar);
+}
+
+function writeText (form) {
+        form.inputbox.value = "Have a nice day!"
+    }
