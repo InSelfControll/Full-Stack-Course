8fa0ed3665ae4207b7cfead03aa9d687cec528a6
BuildTools <unconfigured@null.spigotmc.org>
2021-01-05 11:45:05 +0200

    --- Auto-Git Commit ---

--- homework/EX22.js
+++ homework/EX22.js
@@ -1,7 +1,7 @@
 document.write("<h1>Ex 10 </h1>");
 document.write("<table border='1' style='border-collapse:collapse;font-family:David;font-size:20pt>'");
 alert("how old are you ? ");
-var lines = prompt("Enter number of Lines") ;
+var lines = prompt("Enter number of Lines");
 var lines = parseInt(tmp);
 alert(lines);
 for (var i = 1; i <= lines; i++) {
