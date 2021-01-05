27a4fc4d3e541cb04bb21cb2b7e51f5e0b8545ad
BuildTools <unconfigured@null.spigotmc.org>
2021-01-05 10:14:44 +0200

    --- Auto-Git Commit ---

--- homework/EX22.js
+++ homework/EX22.js
@@ -1,9 +1,10 @@
 const { write } = require("fs");
 
 var name = ofir;
-var pwrite = document.write("<h2>hello");
-for (var i = 0; i < 5 ; i++){
-    pwrite;
+document.write("<h2> Good Morning!</h2>")
+document.write("<body>");
+for (let i = 1; i <= 5 ; i++){
+    document.write("<h2>hello</h2>");
     i++;
 }
-write.document("</h2>")
+document.write("</body>");
