d9bd3f1f62e20c74ebc75a4845c522c5fdd129f3
BuildTools <unconfigured@null.spigotmc.org>
2021-01-05 11:14:58 +0200

    --- Auto-Git Commit ---

--- homework/EX22.js
+++ homework/EX22.js
@@ -1,10 +1,11 @@
-const { write } = require("fs");
-
-var name = ofir;
-document.write("<h2> Good Morning!</h2>")
-document.write("<body>");
-for (let i = 1; i <= 5 ; i++){
-    document.write("<h2>hello</h2>");
-    i++;
+document.write("<h1>Ex 10 </h1>");
+document.write("<table border='1' style='border-collapse:collapse;font-family:David;font-size:20pt>'");
+alert("how old are you ? ");
+var lines = prompt("Enter number of Lines") ;
+var lines = parseInt(tmp);
+alert(lines);
+for (var i = 1; i <= lines; i++) {
+	document.write("<tr><td><img src='/Images/" + i + ".jpg' height= '200' width= '300' /></td>");
+	document.write("<td><a href=" + i + ".html>" + i + " Go to the page</a></td></tr>");
 }
-document.write("</body>");
+document.write("</table>");
